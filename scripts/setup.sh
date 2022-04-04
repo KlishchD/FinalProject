c2ac3bb16299abb8eee279cd3b67cd9a00d09fdb
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-04-04 11:23:06 +0300

    Created generation module for generaion code

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-cp -r ../generators ../Docker/airflow/dags
-mv ../Docker/airflow/dags/generators ../Docker/airflow/dags/utils
-
-cp -r ../dags/* ../Docker/airflow/dags
-
-mkdir ../Docker/airflow/dags/tmp
