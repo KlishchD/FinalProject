912d578da758d2a775f191016af6331a235ae2ae
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-04-29 15:00:59 +0300

    Added update to the table schema if table already exists

---
+++ BigQueryScripts/Utils/update_table_bq.sh
@@ -0,0 +1,1 @@
+bq update gd-gcp-gridu-cloud-cert:results."$1" "$2"
--- BigQueryScripts/set_up_bq.sh
+++ BigQueryScripts/set_up_bq.sh
@@ -3,5 +3,5 @@
 
 for FILE in schemas/*; do
   name="${${FILE:8}:0:-12}";
-  ./Utils/create_table_bq.sh $name $name "$FILE" "ts" DAY
+  ./Utils/create_table_bq.sh $name $name "$FILE" "ts" DAY || ./Utils/update_table_bq.sh $name "$FILE"
 done
