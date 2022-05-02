f8b49d8b0c1a54555b80d49261d01dba3ff56d23
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-05-02 16:37:07 +0300

    Made small fixes

--- BigQueryScripts/set_up_bq.sh
+++ BigQueryScripts/set_up_bq.sh
@@ -1,7 +1,16 @@
+
+bq ls | awk '/results/{print}' | wc -c | read exists
+if [[ $exists == 0 ]]; then
 ./Utils/create_encription_key.sh
 ./Utils/create_data_set.sh
+fi
 
 for FILE in schemas/*; do
   name="${${FILE:8}:0:-12}";
-  ./Utils/create_table_bq.sh $name $name "$FILE" "ts" DAY || ./Utils/update_table_bq.sh $name "$FILE"
+  bq ls results | awk '/ *_aggregation/{print}' | wc -c | read exists
+  if [[ $exists != 0 ]]; then
+    ./Utils/update_table_bq.sh $name "$FILE"
+  else
+    ./Utils/create_table_bq.sh $name $name "$FILE" "ts" DAY
+  fi
 done
